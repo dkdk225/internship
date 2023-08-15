c3c8ea653ee0c098a5c1e83085d56c76e63b5370
Meltem TEKELİ AKDAĞ <meltem.akdag@enelsa.com>
2023-08-15 10:14:23 +0300

    feat: create module for mqtt connection

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -1,9 +1,17 @@
 const mqtt = require('mqtt')
+const {mqttUser} = require('../config');
 const {mqttUser} = require('../config')
 module.exports = mqtt.connectAsync(
-    mqttUser.url,
-    {
-        username:mqttUser.username,
-        password:mqttUser.password
-    }    
+
+
+
+module.exports = {
+    mqtt:mqtt.connectAsync(
+        mqttUser.url,
+        {
+            username:mqttUser.username,
+            password:mqttUser.password
+        }    
+    ),
+);}
 );
