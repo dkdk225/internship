549642ffc3ed9f2c69fa6fb6653a89e109444d9d
Meltem TEKELİ AKDAĞ <meltem.akdag@enelsa.com>
2023-08-15 10:54:51 +0300

    feat: create an api for getting an encrypted message

--- router/index.js
+++ router/index.js
@@ -1,15 +1,14 @@
 const {Router} = require('express')
 const {encrypt, decrypt} = require('../encryption')
-const mqtt = require('../mqtt')
+const {mqtt} = require('../mqtt')
 const { v4:uuidv4 } = require('uuid');
-
 const path = require('path')
-
-
 const index = Router({
     caseSensitive: true,
 
 })
+
+
 
 index.get('/',(req, res)=>{
     //set header values to tell browser document type
@@ -23,21 +22,46 @@ index.get('/',(req, res)=>{
     )
 })
 
+
 index.post('/',(req,res)=>{
     const id = uuidv4();
     const {message, topic} = req.body
     const topicEncrypted = JSON.stringify(encrypt(topic))
     const messageEncrypted = JSON.stringify(encrypt(message))
-        mqtt.then(broker => {
-            broker.publish(
-                `testing/${topicEncrypted}/${id}`,
-                 messageEncrypted,
-                 {
-                    qos:2
-                 }
-            )
-        })
-    res.end()
+    
+    mqtt.then(broker => {
+        broker.publish(
+            `testing/${topicEncrypted}/${id}`,
+            '',
+            {
+                qos:2
+            },
+            (err)=>{
+                broker.subscribe(
+                    `testing/${topicEncrypted}/+`,
+                    (err, {topic, qos})=>{
+                        console.log(err)
+    
+                    }
+                )
+    
+                broker.publish(
+                    `testing/${topicEncrypted}/${id}`,
+                    messageEncrypted,
+                    {
+                        qos:2
+                    },
+                    (err)=>{
+                        console.log(messageEncrypted)
+                        res.send('published successfully')
+                    }
+                )
+            }
+        )
+    })
+
+        
+
 })
 
 
