c4515853e114e855961e5c5fa3adf608e641ac0d
Meltem TEKELİ AKDAĞ <meltem.akdag@enelsa.com>
2023-08-15 10:08:24 +0300

    refactor: encrypt to base64 instead of buffer since it works better with JSON.stringify

--- encryption/encryption.js
+++ encryption/encryption.js
@@ -9,17 +9,17 @@ function encrypt(data){
     let encryptedPayload = Buffer.concat([cipher.update(data, 'utf-8'), cipher.final()])
     const authTag = cipher.getAuthTag()
     return {
-        encryptedPayload: encryptedPayload.toString('hex'),
+        encryptedPayload: encryptedPayload.toString('base64'),
         iv: iv,
-        authTag: authTag.toString('hex')
+        authTag: authTag.toString('base64')
     }
 }
 
 function decrypt({encryptedPayload, iv, authTag}){
 
     const decipher = createDecipheriv('aes-256-gcm', secretKey, iv)
-    decipher.setAuthTag(authTag, 'hex')
-    const decryptedPayload = Buffer.concat([decipher.update(encryptedPayload, 'hex'), decipher.final()]);
+    decipher.setAuthTag(authTag, 'base64')
+    const decryptedPayload = Buffer.concat([decipher.update(encryptedPayload, 'base64'), decipher.final()]);
     
     return decryptedPayload
 }
