4050952153c18319807a9a9c028ddc23c6267eda
Meltem TEKELİ AKDAĞ <meltem.akdag@enelsa.com>
2023-08-21 11:07:13 +0300

    feat(controllJWT.js): extend JWT controller in order to add the token sender to req as jwtSender

--- middleware/controllJWT.js
+++ middleware/controllJWT.js
@@ -2,11 +2,15 @@ const {JWTController} = require('../jwt_controller')
 module.exports = function () {
     return (req, res, next)=>{
         req.jwt = false
+        req.jwtSender = null
         const auth = req.get('Authorization')
         try {
             const token = auth.substring(7)
-            if(JWTController.validateJWT(token))
+            const sender = JWTController.validateJWT(token)
+            if(sender){
                 req.jwt = true
+                req.jwtSender = sender
+            }
         } catch (error) {
             console.log("ERROR at jwt controll")
             console.log(error)
